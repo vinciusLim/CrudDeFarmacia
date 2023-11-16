1be7ee2c90864a24c4392325b7c3e4130588f1df
vinciusLim <vinicius2008852@gmail.com>
2023-11-16 10:44:51 -0300

    vvd

---
+++ src/main/java/CRUD/Funcionario/Departamentos.java
@@ -0,0 +1,12 @@
+package CRUD.Funcionario;
+
+public enum Departamentos {
+    TI,
+    VENDAS,
+    ADMINISTRATIVO
+
+    /* Quando for perguntar o departamento no main, é so pedir em formato de String utilizando o Departamentos
+     + value.of("nome do departamento"), EX.: Departamentos.value.of("VENDAS"). se lembrar de usar uppercase
+     na string.
+     */
+}
--- src/main/java/CRUD/Funcionario/Funcionario.java
+++ src/main/java/CRUD/Funcionario/Funcionario.java
@@ -1,39 +1,64 @@
 package CRUD.Funcionario;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class Funcionario {
-  private static int proximoId = 1;
-  private int idFuncionario;
-  private String nomeFuncionario;
-  private double salarioFuncionario;
+    private static int proximoId = 1;
+    private int idFuncionario;
+    private String nomeFuncionario;
+    private double salarioFuncionario;
+    private Instant dataCadastro;
+    private LocalDate dataAdmissao;
+    private Departamentos departamento;
 
-  public Funcionario(String nomeFuncionario, double salarioFuncionario) {
-    this.nomeFuncionario = nomeFuncionario;
-    this.salarioFuncionario = salarioFuncionario;
-    this.idFuncionario = proximoId;
-    proximoId++;
-  }
+    public Funcionario(String nomeFuncionario, double salarioFuncionario, LocalDate dataAdmissao, Departamentos departamento) {
+        this.idFuncionario = proximoId;
+        proximoId++;
+        this.nomeFuncionario = nomeFuncionario;
+        this.salarioFuncionario = salarioFuncionario;
+        this.dataCadastro = Instant.now();
+        this.dataAdmissao = dataAdmissao;
+        this.departamento = departamento;
+    }
 
-  public static int getProximoId() {
-    return proximoId;
-  }
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
 
-  public int getIdFuncionario() {
-    return idFuncionario;
-  }
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
 
-  public String getNomeFuncionario() {
-    return nomeFuncionario;
-  }
+    public double getSalarioFuncionario() {
+        return salarioFuncionario;
+    }
 
-  public double getSalarioFuncionario() {
-    return salarioFuncionario;
-  }
 
-  @Override
-  public String toString() {
-    return "Funcionario: " +
-            "ID = " + idFuncionario +
-            ", nome = " + nomeFuncionario + '\'' +
-            ", salario = " + salarioFuncionario;
-  }
+    public Instant getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public Departamentos getDepartamento() {
+        return departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "idFuncionario=" + idFuncionario +
+                ", nomeFuncionario='" + nomeFuncionario + '\'' +
+                ", salarioFuncionario=" + salarioFuncionario +
+                ", dataCadastro=" + DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withZone(ZoneId.of("-03:00")).format(dataCadastro) +
+                ", dataAdmissao=" + DateTimeFormatter.ofPattern("dd/MM/yyyy").format(dataAdmissao) +
+                ", departamento='" + departamento + '\'' +
+                '}';
+    }
 }
