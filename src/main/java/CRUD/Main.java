66be3ea44f32ab2629ef64541a437fa4ec720e74
Vinícius Lima <86134409+vinciusLim@users.noreply.github.com>
2023-11-30 10:09:35 -0300

    Update Main.java

--- src/main/java/CRUD/Main.java
+++ src/main/java/CRUD/Main.java
@@ -1,23 +1,53 @@
 package CRUD;
 
-    import CRUD.Funcionario.Funcionario;
+import CRUD.Funcionario.Departamentos;
+import CRUD.Funcionario.Funcionario;
 
-    import java.time.LocalDate;
-    import java.time.LocalDateTime;
-    import java.util.ArrayList;
-    import java.util.Date;
-    import java.util.List;
-    import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    public class Main {
-        public static void main(String[] args) {
-            List <Funcionario> listaDeFuncionario = new ArrayList<>();
-            Funcionario f1 = new Funcionario("cini",520, "1245", LocalDate.of(2012,3,12), "Vendas");
-            Funcionario f2 = new Funcionario("pol",2012, "2563", LocalDate.of(2022,8,14), "Auxiliar Limpeza");
-            listaDeFuncionario.add(f1);
-            listaDeFuncionario.add(f2);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Funcionario> listaDeFuncionarios = new ArrayList<>();
+        int op = 0;
+        String nome, cpf, departamento;
+        double salario;
+        int dia, mes, ano;
 
-            System.out.println(listaDeFuncionario);
+        do {
+            System.out.println("1 - Cadastro de funcionario");
+            op = scanner.nextInt();
 
-        }
+            switch (op) {
+                case 1:
+                    System.out.println("1 - Cadastrar funcionario\n2 - C");
+                    op = scanner.nextInt();
+                    switch (op) {
+                        case 1:
+                            scanner.nextLine();
+                            System.out.println("Digitar nome:");
+                            nome = scanner.nextLine();
+                            System.out.println("Digitar salario:");
+                            salario = scanner.nextDouble();
+                            System.out.println("Digitar dia, mes e ano da data de admissão:");
+                            dia = scanner.nextInt();
+                            mes = scanner.nextInt();
+                            ano = scanner.nextInt();
+                            System.out.println("Digitar Departamento:");
+                            scanner.nextLine();
+                            departamento = scanner.nextLine();
+
+                            listaDeFuncionarios.add(new Funcionario(nome, salario, LocalDate.of(ano, mes, dia), Departamentos.valueOf(departamento.toUpperCase())));
+
+                        for (Funcionario f: listaDeFuncionarios) {
+                            System.out.println(listaDeFuncionarios);
+                        }
+                        break;
+                    }
+            }
+        } while (op != 0);
     }
+}
